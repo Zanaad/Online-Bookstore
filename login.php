81cfe3245fdbeead9f0acee121e7836310c84ce4
Adea Lluhani <131053382+adealluhani@users.noreply.github.com>
2024-04-20 18:15:14 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,25 +5,10 @@ $validator = new LoginValidation();
 
 session_start(); 
 
-
-if (isset($_POST['logout'])) {
-    
-    unset($_SESSION['logged_in']);
-    $_SESSION = array();
-  
-
-    session_destroy();
-  
-
-    header("Location: login.php");
-    exit();
-}
-
-
+$logoutMessage = ""; 
 if(isset($_SESSION['login_timer']) && $_SESSION['login_timer'] > time()) {
-    
     $validator->setLoggedin("You cannot log in again so soon. Please wait.");
-   
+    
 }
 
 if (isset($_POST['login'])) {
@@ -34,11 +19,8 @@ if (isset($_POST['login'])) {
         $userData = $_SESSION['users'][$email];
         $storedPassword = $userData['password'];
 
-        
         if ($password === $storedPassword) {
             $validator->setLoggedin("You have logged in successfully");
-
-         
             $_SESSION['login_timer'] = time() + 30; 
             header("Location: home.php");
             exit();
@@ -46,74 +28,86 @@ if (isset($_POST['login'])) {
             $validator->setPasswordErr("Incorrect email or password");
         }
     } else {
-        $validator->setPasswordErr("User not found.Register!!");
+        $validator->setPasswordErr("User not found. Register!!");
     }
 }
 
+if(isset($_GET['message'])) {
+    $message = $_GET['message'];
 
-header("Location: login.php");
-exit();
+    echo "<script>alert('$message');</script>";
+}
+
+
+if(isset($_POST['logout'])) {
+   
+    unset($_SESSION['users']);
+   
+    
+    $logoutMessage = "You have been logged out."; 
+  
+}
 ?>
 
-
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Log in</title>
-
-  <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
-
-  <link rel="stylesheet" href="login.css">
-  <meta name="robots" content="noindex, follow">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="login.css">
+    <meta name="robots" content="noindex, follow">
 </head>
-
 <body>
-  <div class="main">
-    <section class="sign-in">
-      <div class="container">
-        <div class="signin-content">
-          <div class="signin-image">
-            <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
-            <a href="signup.php" class="signup-image-link">Create an account</a>
-          </div>
-          <div class="signin-form">
-            <h2 class="form-title">Log in</h2>
-            <form method="POST" class="register-form" id="login-form">
-              <div class="form-group">
-                <label for="your_email"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                <input type="email" name="name" id="your_email" placeholder="Email">
-                <span style="color: red;"><?php echo $validator->getEmailErr(); ?></span>
-              </div>
-              <div class="form-group">
-                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                <input type="password" name="password" id="your_pass" placeholder="Password">
-                <span style="color:red"><?php echo $validator->getPasswordErr(); ?></span>
-              </div>
-              <div class="form-group">
-                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term">
-                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
-              </div>
-              <div class="form-group form-button">
-                <input type="submit" name="login" id="signin" class="form-submit" value="Log in">
-                <span style="color: green;"><?php echo $validator->getLoggedin(); ?></span>
-              </div>
-            </form>
-            <div class="social-login">
-              <span class="social-label">Or login with</span>
-              <ul class="socials">
-                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
-                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
-              </ul>
+    <div class="main">
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                        <a href="signup.php" class="signup-image-link">Create an account</a>
+                    </div>
+                    <div class="signin-form">
+                        <h2 class="form-title">Log in</h2>
+                        <form method="POST" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="your_email"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="email" name="email" id="your_email" placeholder="Email">
+                                <span style="color: red;"><?php echo $validator->getEmailErr(); ?></span>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="your_pass" placeholder="Password">
+                                <span style="color:red"><?php echo $validator->getPasswordErr(); ?></span>
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term">
+                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="login" id="signin" class="form-submit" value="Log in" style="margin-top: 30px;">
+                                <span style="color: green;"><?php echo $validator->getLoggedin(); ?></span>
+                                <input type="submit" name="logout" id="signout" class="form-submit" value="Log out" style="margin-top: -150px; margin-top: -80px; margin-left:140px;">
+                                <span style="color: red;"><?php echo $logoutMessage; ?></span>
+                            </div>
+                           
+                        </form>
+                       
+                        <div class="social-login">
+                            <span class="social-label">Or login with</span>
+                            <ul class="socials">
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+        </section>
+    </div>
 </body>
-
 </html>
+
