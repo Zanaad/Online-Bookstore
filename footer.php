4c209f96bd4f56e4de4541e941523cd30f0b23f0
Zana <zana.ademi@student.uni-pr.edu>
2024-05-21 00:49:46 +0200

    wip

--- footer.php
+++ footer.php
@@ -1,41 +1,36 @@
-<section class="footer">
-
-   <div class="box-container">
-
-      <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php">home</a>
-         <a href="about.php">about</a>
-         <a href="shop.php">shop</a>
-         <a href="contact.php">contact</a>
+<footer>
+   <div class="content">
+      <div class="top">
+         <div class="logo-details">
+            <i class="fab fa-slack"></i>
+            <span class="logo_name">Libraria Living</span>
+         </div>
+         <div class="media-icons">
+            <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+         </div>
       </div>
-
-      <div class="box">
-         <h3>extra links</h3>
-         <a href="login.php">login</a>
-         <a href="register.php">register</a>
-         <a href="cart.php">cart</a>
-         <a href="orders.php">orders</a>
+      <div class="link-boxes">
+         <ul class="box">
+            <li class="link_name">LIBRARIA LIVING</li>
+            <li><a href="#">Bulevardi Nene Tereza</a></li>
+            <li><a href="mailto:bookstore@gmail.com">bookstore@gmail.com</a></li>
+            <li><a href="callto:+38345647763">+383 45 647 763</a></li>
+         </ul>
+         <ul class="box">
+            <li class="link_name">Te duhet ndihme</li>
+            <li><a href="about.php">Rreth nesh</a></li>
+            <li><a href="contact.php">Na kontakto</a></li>
+            <li class="link_name">
+               Kontakto
+               <abbr title="Kontakto sherbimin e konsumatorit">SHEK</abbr>
+            </li>
+         </ul>
+         <ul class="box input-box">
+            <li class="link_name">Subscribe</li>
+            <li><input type="text" placeholder="Enter your email" /></li>
+         </ul>
       </div>
-
-      <div class="box">
-         <h3>contact info</h3>
-         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
-         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
-         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
-         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
-      </div>
-
-      <div class="box">
-         <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-      </div>
-
    </div>
-
-   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
-
-</section>
+</footer>
--- header.php
+++ header.php
@@ -1,14 +1,39 @@
 <?php
-if(isset($message)){
-   foreach($message as $message){
+if (isset($message)) {
+   foreach ($message as $message) {
       echo '
       <div class="message">
-         <span>'.$message.'</span>
+         <span>' . $message . '</span>
          <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
       </div>
       ';
    }
 }
+
+include 'config.php';
+
+$user_id = $_SESSION['user_id'];
+
+if (!isset($user_id)) {
+   header('location:login.php');
+};
+
+if (isset($_POST['add_to_cart'])) {
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if (mysqli_num_rows($check_cart_numbers) > 0) {
+      $message[] = 'already added to cart!';
+   } else {
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message[] = 'product added to cart!';
+   }
+};
 ?>
 
 <header class="header">
@@ -27,23 +52,31 @@ if(isset($message)){
 
    <div class="header-2">
       <div class="flex">
-         <a href="home.php" class="logo">Bookly.</a>
+         <a href="home.php" class="logo">Libraria Living</a>
+
+         <div class="search-container">
+            <form action="" class="search" method="post">
+               <input type="text" class="search-bar" placeholder="Search..." name="search">
+               <button type="submit" class="search-button" name="submit"> <i class="fas fa-search search-icon"></i></button>
+            </form>
+         </div>
+
+         </section>
 
          <nav class="navbar">
-            <a href="home.php">home</a>
-            <a href="about.php">about</a>
-            <a href="shop.php">shop</a>
-            <a href="contact.php">contact</a>
-            <a href="orders.php">orders</a>
+            <a href="shop.php">Books</a>
+            <a href="orders.php">Orders</a>
+            <a href="contact.php">Contact Us</a>
+            <a href="about.php">About Us</a>
          </nav>
 
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
-            <a href="search_page.php" class="fas fa-search"></a>
+
             <div id="user-btn" class="fas fa-user"></div>
             <?php
-               $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-               $cart_rows_number = mysqli_num_rows($select_cart_number); 
+            $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+            $cart_rows_number = mysqli_num_rows($select_cart_number);
             ?>
             <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
          </div>
@@ -55,5 +88,35 @@ if(isset($message)){
          </div>
       </div>
    </div>
-
 </header>
+
+<section class="products" style="padding-top: 0;">
+
+   <div class="box-container">
+      <?php
+      if (isset($_POST['submit'])) {
+         $search_item = $_POST['search'];
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE name LIKE '%{$search_item}%'") or die('query failed');
+         if (mysqli_num_rows($select_products) > 0) {
+            while ($fetch_product = mysqli_fetch_assoc($select_products)) {
+      ?>
+               <form action="" method="post" class="box">
+                  <img src="uploaded_img/<?php echo $fetch_product['image']; ?>" alt="" class="image">
+                  <div class="name"><?php echo $fetch_product['name']; ?></div>
+                  <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
+                  <input type="number" class="qty" name="product_quantity" min="1" value="1">
+                  <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
+                  <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
+                  <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
+                  <input type="submit" class="btn" value="add to cart" name="add_to_cart">
+               </form>
+      <?php
+            }
+         } else {
+            echo '<p class="empty">no result found!</p>';
+         }
+      }
+      ?>
+   </div>
+
+</section>
