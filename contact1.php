42d185bc431014b737f75c052c286e99e36334e4
Adea Lluhani <131053382+adealluhani@users.noreply.github.com>
2024-05-26 23:55:52 +0200

    error handlers

--- contact1.php
+++ contact1.php
@@ -1,9 +1,15 @@
 <?php
 
 
+function custom_error_handler($errno, $errstr, $errfile, $errline) {
+    echo "Ndodhi nje gabim: $errstr ne $errfile ne rreshtin $errline";
+}
+
+set_error_handler("custom_error_handler");
+
+
 function custom_exception_handler($exception) {
-    
-    echo "An error occurred: " . $exception->getMessage();
+    echo "Ndodhi një gabim: " . $exception->getMessage();
 }
 
 
@@ -11,55 +17,52 @@ set_exception_handler("custom_exception_handler");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
-       
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
-
         
-        if (empty($name) || empty($email) || empty($message)) {
+        $emri = $_POST["name"];
+        $email = $_POST["email"];
+        $mesazhi = $_POST["message"];
+
+       
+        if (empty($emri) || empty($email) || empty($mesazhi)) {
             throw new Exception("Mbushni te gjitha fushat");
         }
 
-       
-        $to = "?";
-        $subject = "Form Submission";
-        $body = "Emri: $name\nEmail: $email\nMesazhi:\n$message";
-
         
-        if (!mail($to, $subject, $body)) {
-            throw new Exception("Gabim: Mesazhi i juaj nuk u dergua me sukses");
+        $to = "adealluhani@gmail.com";
+        $subjekti = "Dergimi i Formes";
+        $trupi = "Emri: $emri\nEmail: $email\nMesazhi:\n$mesazhi";
+
+        if (!mail($to, $subjekti, $trupi)) {
+            throw new Exception("Gabim: Mesazhi juaj nuk u dergua me sukses");
         }
 
-        //nonexistent_folder/messages.txt
-        $file_path = "nonexistent_folder/messages.txt";
-        $file_content = "Emri: $name\nEmail: $email\nMesazhi: $message\n\n";
+        //folder_paekzistues/mesazhet.txt
+        $file_path = "contact_messages.txt";
+        $file_content = "Emri: $emri\nEmail: $email\nMesazhi: $mesazhi\n\n";
 
        
         $file = fopen($file_path, "a");
         if (!$file) {
-            
-            throw new Exception("Gabim: Nuk mund të hapet file për ruajtjen e mesazhit.");
+            throw new Exception("Gabim: Nuk mund te hapet file per ruajtjen e mesazhit.");
         }
 
-        
+       
         if (!fwrite($file, $file_content)) {
-           
-            throw new Exception("Gabim: Nuk mund të shkruhet në file për ruajtjen e mesazhit.");
+            throw new Exception("Gabim: Nuk mund te shkruhet ne file per ruajtjen e mesazhit.");
         } else {
-           
             fclose($file);
         }
 
-        
+      
         echo "Ju faleminderit! Mesazhi u dergua dhe u ruajt.";
     } catch (Exception $e) {
-       
+        
         custom_exception_handler($e);
     }
 }
 
 ?>
+
 
 
 
