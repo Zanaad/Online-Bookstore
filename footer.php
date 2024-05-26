03dec3df2bc8083160cc5852f7e21ebf378d468e
Zana <zana.ademi@student.uni-pr.edu>
2024-05-26 23:51:16 +0200

    footer file

---
+++ footer.php
@@ -0,0 +1,37 @@
+  <footer>
+   <div class="content">
+    <div class="top">
+     <div class="logo-details">
+      <i class="fab fa-slack"></i>
+      <span class="logo_name">Libraria Living</span>
+     </div>
+     <div class="media-icons">
+      <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+      <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+
+     </div>
+    </div>
+    <div class="link-boxes">
+     <ul class="box">
+      <li class="link_name">LIBRARIA DUKAGJINI</li>
+      <li><a href="#">Bulevardi Nene Tereza</a></li>
+      <li><a href="mailto:bookstore@gmail.com">bookstore@gmail.com</a></li>
+      <li><a href="callto:+383 45 647 763">+383 45 647 763</a></li>
+
+     </ul>
+     <ul class="box">
+      <li class="link_name">Te duhet ndihme</li>
+      <li><a href="about.php">Rreth nesh</a></li>
+      <li><a href="contact.php">Na kontakto</a></li>
+      <li><a href="#">Politikat e privatesise</a></li>
+     </ul>
+
+
+     <ul class="box input-box">
+      <li class="link_name">Subscribe</li>
+      <li><input type="text" placeholder="Enter your email"></li>
+     </ul>
+    </div>
+   </div>
+  </footer>
--- frengjisht.php
+++ frengjisht.php
@@ -43,43 +43,9 @@
     ?>
   </div>
 
-  <footer>
-    <div class="content">
-      <div class="top">
-        <div class="logo-details">
-          <i class="fab fa-slack"></i>
-          <span class="logo_name">Libraria Living</span>
-        </div>
-        <div class="media-icons">
-          <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
-          <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
-
-        </div>
-      </div>
-      <div class="link-boxes">
-        <ul class="box">
-          <li class="link_name">LIBRARIA LIVING</li>
-          <li><a href="#">Bulevardi Nene Tereza</a></li>
-          <li><a href="mailto:bookstore@gmail.com">bookstore@gmail.com</a></li>
-          <li><a href="callto:+383 45 647 763">+383 45 647 763</a></li>
-
-        </ul>
-        <ul class="box">
-          <li class="link_name">Te duhet ndihme</li>
-          <li><a href="about.php">Rreth nesh</a></li>
-          <li><a href="contact.php">Na kontakto</a></li>
-          <li class="link_name">Kontakto <abbr title="Kontakto sherbimin e konsumatorit">SHEK</abbr></li>
-        </ul>
-
-        <ul class="box input-box">
-          <li class="link_name">Subscribe</li>
-          <li><input type="text" placeholder="Enter your email"></li>
-        </ul>
-      </div>
-    </div>
-  </footer>
-
+  <?php
+  include 'footer.php'
+  ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
