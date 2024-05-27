83a8227f65307701f8482998be13e6f4ace6c6eb
Zana <zana.ademi@student.uni-pr.edu>
2024-05-27 19:06:24 +0200

    updates

--- add_to_cart.php
+++ add_to_cart.php
@@ -2,13 +2,6 @@
 session_start();
 include 'db_connect.php';
 
-// Check if the request is an AJAX request
-if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
- // If not an AJAX request, deny access
- http_response_code(403);
- exit('Forbidden');
-}
-
 if (!isset($_SESSION['user_id'])) {
  echo json_encode(['status' => 'error', 'message' => 'User not logged in']);
  exit();
@@ -16,7 +9,6 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 
-// Retrieve book_id from AJAX request data
 $book_id = isset($_POST['book_id']) ? $_POST['book_id'] : null;
 
 if (!$book_id) {
