a839c76325620aad1005d57afca4d5f32ba1a80d
Zana <zana.ademi@student.uni-pr.edu>
2024-05-27 16:16:12 +0200

    changes

--- login.php
+++ login.php
@@ -9,16 +9,10 @@ if (isset($_POST['submit'])) {
    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
    // Fetch user data based on email
-  // $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('Query failed');
-$stmt=$conn->prepare("SELECT * FROM 'users' WHERE EMAIL = ?");
-$stmt->bind_param('s', $email);
-$stmt->execute();
-$result=$stmt->get_result();
-if($result->num_rows>0){
-   $row=$result->fetch_assoc();
-}
-  // if (mysqli_num_rows($select_users) > 0) {
-    //  $row = mysqli_fetch_assoc($select_users);
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('Query failed');
+
+   if (mysqli_num_rows($select_users) > 0) {
+      $row = mysqli_fetch_assoc($select_users);
       // Extract the salt and hashed password from the fetched user data
       $salt = $row['salt'];
       $hashed_password = $row['hashPassword'];
@@ -49,6 +43,55 @@ if($result->num_rows>0){
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <?php
+   if (isset($message)) {
+      foreach ($message as $message) {
+         echo '
+      <div class="message">
+         <span>' . $message . '</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+      }
+   }
+   ?>
+
+   <div class="form-container">
+
+      <form action="" method="post">
+         <h3>Login Now</h3>
+         <input type="email" name="email" placeholder="Enter your email" required class="box">
+         <input type="password" name="password" placeholder="Enter your password" required class="box">
+         <input type="submit" name="submit" value="Login Now" class="btn">
+         <p>Don't have an account? <a href="signup.php">Register Now</a></p>
+      </form>
+
+   </div>
+
+</body>
+
+</html>
+
 
 <!DOCTYPE html>
 <html lang="en">
