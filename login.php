20cff705d35bd2ede21bacd66a0046f74b6b24c9
AgnesaHulaj2 <154357099+AgnesaHulaj2@users.noreply.github.com>
2024-05-28 20:29:07 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 
 include './php/db_connect.php';
 session_start();
@@ -46,8 +47,67 @@ if (isset($_POST['submit'])) {
    $stmt->close();
    $conn->close();
 }
+=======
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the email address from the POST parameters
+    $email = $_POST['email'];
 
+    // Validate the email address
+    if (validateEmail($email)) {
+        // Redirect to the change password page
+        header('Location: changePassword.php?email=' . urlencode($email));
+        exit;
+    } else {
+        // Email is invalid, redirect back to the form with an error message
+        $error = 'Invalid email. Please try again.';
+        header("Location: verify_email.php?error=" . urlencode($error) . "&email=" . urlencode($email));
+        exit;
+    }
+} else {
+    // Invalid request method, redirect back to the form
+    header('Location: verify_email.php');
+    exit;
+}
+
+function validateEmail($email)
+{
+    // Connect to the database
+    $servername = "localhost:3307";
+    $username = "root";
+    $password = "";
+    $dbname = "bookstore_db";
+>>>>>>> Stashed changes
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare the query to validate the email address
+    $sql = "SELECT * FROM admin WHERE email_address = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+
+    // Execute the query
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Check if a matching row is found
+    if ($result->num_rows > 0) {
+        // Email is valid
+        $stmt->close();
+        $conn->close();
+        return true;
+    } else {
+        // Email is invalid
+        $stmt->close();
+        $conn->close();
+        return false;
+    }
+}
 ?>
+<<<<<<< Updated upstream
 
 <!DOCTYPE html>
 <html lang="en">
@@ -91,3 +151,5 @@ if (isset($_POST['submit'])) {
 </body>
 </html>
 
+=======
+>>>>>>> Stashed changes
