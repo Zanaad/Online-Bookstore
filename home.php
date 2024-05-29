5ef1d84bde6eec1cb17cb1efe0e7c66ffe50e7f1
Yllka Kastrati <117120519+Yllka5@users.noreply.github.com>
2024-05-29 16:52:17 +0200

    home.php

--- home.php
+++ home.php
@@ -1086,6 +1086,8 @@
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
    <script src="script.js"></script>
+     <script src="ajax-requests.js"></script>
+
  </body>
 
  </html>
